8dc1c0d40b685e6949df12290362261c33757473
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2023-06-27 14:15:25 +0530

    Update SASTConstants.java

--- src/main/java/com/hcl/appscan/sdk/scanners/sast/SASTConstants.java
+++ src/main/java/com/hcl/appscan/sdk/scanners/sast/SASTConstants.java
@@ -1,6 +1,6 @@
 /**
  * © Copyright IBM Corporation 2016.
- * © Copyright HCL Technologies Ltd. 2017. 
+ * © Copyright HCL Technologies Ltd. 2017, 2023. 
  * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
 
@@ -31,9 +31,9 @@ public interface SASTConstants {
 	String THIRD_PARTY					= "thirdParty";						//$NON-NLS-1$
         String OPEN_SOURCE_ONLY                                  = "openSourceOnly";                                     //$NON-NLS-1$
         String SOURCE_CODE_ONLY                                  = "sourceCodeOnly";                                     //$NON-NLS-1$
-    String SCAN_SPEED                   = "scanSpeed";                      //$NON-NLS-1$
-    String OPT_SCAN_SPEED               = "-s";                             //$NON-NLS-1$
-    String OPT_NAME						= "-n";								//$NON-NLS-1$
+    	String SCAN_SPEED                                        = "scanSpeed";                                          //$NON-NLS-1$
+    	String OPT_SCAN_SPEED                                    = "-s";                                                 //$NON-NLS-1$
+    	String OPT_NAME						= "-n";								//$NON-NLS-1$
 	String OPT_SAVE_LOCATION			= "-d";								//$NON-NLS-1$
 	String OPT_LOG_LOCATION				= "-l";								//$NON-NLS-1$
 	String OPT_CONFIG					= "-c";								//$NON-NLS-1$

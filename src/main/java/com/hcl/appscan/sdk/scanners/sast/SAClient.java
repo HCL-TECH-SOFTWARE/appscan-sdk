00f93feb0c2575eaa11ce0f1bdd9f4640ddb4d8a
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2023-07-28 22:49:01 +0530

    ASA 6609 & ASA 6586 (#94)
    ASA-6586 
    ASA-6609
    Handled the serverURL behavior.

--- src/main/java/com/hcl/appscan/sdk/scanners/sast/SAClient.java
+++ src/main/java/com/hcl/appscan/sdk/scanners/sast/SAClient.java
@@ -108,13 +108,16 @@ public class SAClient implements SASTConstants {
 			m_builder.environment().put(IRGEN_CLIENT_PLUGIN_VERSION, irgenClientPluginVersion);
 			
 		m_progress.setStatus(new Message(Message.INFO, Messages.getMessage(PREPARING_IRX, getLocalClientVersion())));
-                if((serverURL !=null) && !serverURL.isEmpty()){
-                    String server = "-DBLUEMIX_SERVER="+serverURL;
-                    if(acceptInvalidCerts != null && acceptInvalidCerts.equals("true")){
-                        server = server+" -Dacceptssl";
-                    }
-                    m_builder.environment().put("APPSCAN_OPTS",server);
-                }
+        if((serverURL !=null) && !serverURL.isEmpty()){
+            String options = System.getenv(CoreConstants.APPSCAN_OPTS) == null ? "" : System.getenv(CoreConstants.APPSCAN_OPTS);
+            if(!options.contains(CoreConstants.BLUEMIX_SERVER)) {
+                options += " -DBLUEMIX_SERVER=" + serverURL;
+            }
+            if("true".equals(acceptInvalidCerts)) {
+                options += " -Dacceptssl";
+            }
+            m_builder.environment().put(CoreConstants.APPSCAN_OPTS, options);
+        }
 
                 final Process proc = m_builder.start();
 		    new Thread(new Runnable() {

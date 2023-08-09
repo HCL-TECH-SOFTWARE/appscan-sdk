5a4141065d86ddd0b7c8c4ccd5f34056a6a490ac
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2023-08-09 23:23:58 +0530

    Update ArchiveUtil.java

--- src/main/java/com/hcl/appscan/sdk/utils/ArchiveUtil.java
+++ src/main/java/com/hcl/appscan/sdk/utils/ArchiveUtil.java
@@ -123,7 +123,7 @@ public class ArchiveUtil {
     }
 
     private static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
-        if (fileToZip.isHidden() || (fileToZip.isDirectory() && fileToZip.getName().equals(".git"))) {
+        if (fileToZip.getName().startsWith(".")) {
             return;
         }
         if (fileToZip.isDirectory()) {

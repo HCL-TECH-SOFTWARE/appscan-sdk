1b0563df14d9749fd496b524d37a87f5ef65df04
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2025-08-12 18:17:24 +0530

    suggested changes

--- src/main/java/com/hcl/appscan/sdk/utils/ServiceUtil.java
+++ src/main/java/com/hcl/appscan/sdk/utils/ServiceUtil.java
@@ -349,4 +349,28 @@ public class ServiceUtil implements CoreConstants {
         }
             return false;
     }
+
+    public static boolean compareVersions(String baseVersion, String newVersion) {
+        if(baseVersion == null)
+            return true;
+
+        if(newVersion != null) {
+            String[] base = baseVersion.split("\\."); //$NON-NLS-1$
+            String[] next = newVersion.split("\\."); //$NON-NLS-1$
+
+            try {
+                for(int iter = 0; iter < base.length && iter < next.length; iter++) {
+                	int lVersion = Integer.parseInt(base[iter]);
+                	int sVersion = Integer.parseInt(next[iter]);
+
+                	if (((iter==0) && lVersion<sVersion) || (iter==1 && lVersion<sVersion) || (iter==2 && lVersion<sVersion)) {
+                		return true;
+                	}
+                }
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return false;
+    }
 }
